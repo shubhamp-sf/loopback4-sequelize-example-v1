cb1804b4c8c330b78bbc5940dda0acb4d22ad1d2
Shubham Prajapat <shubham.prajapat@sourcefuse.com>
2022-12-06 10:32:57 +0530

    add todo-list model and repository

---
+++ src/repositories/todo-list.repository.ts
@@ -0,0 +1,16 @@
+import {inject} from '@loopback/core';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {DbDataSource} from '../datasources';
+import {TodoList, TodoListRelations} from '../models';
+
+export class TodoListRepository extends DefaultCrudRepository<
+  TodoList,
+  typeof TodoList.prototype.id,
+  TodoListRelations
+> {
+  constructor(
+    @inject('datasources.db') dataSource: DbDataSource,
+  ) {
+    super(TodoList, dataSource);
+  }
+}
