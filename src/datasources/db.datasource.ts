dd3b76810d26d652dc328c73935d04732a1ba29a
Shubham Prajapat <shubham.prajapat@sourcefuse.com>
2022-12-06 10:10:29 +0530

    add todo model, repository and datasource

---
+++ src/datasources/db.datasource.ts
@@ -0,0 +1,30 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'db',
+  connector: 'postgresql',
+  host: 'localhost',
+  port: 5999,
+  user: 'postgres',
+  password: 'rest-rest',
+  database: 'postgres'
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class DbDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'db';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.db', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
